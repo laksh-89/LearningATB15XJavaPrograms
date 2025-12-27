d2d353f8e5bd8871b55fff674fbdd72c216893fd
laksh-89 <vermalaksh1@gmail.com>
2025-12-27 21:37:47 +0530

    Add 2 programs

---
+++ src/ex_04_Operators/Op_Instanceof.java
@@ -0,0 +1,9 @@
+package ex_04_Operators;
+
+public class Op_Instanceof {
+    public static void main(String[] args) {
+        String s1= new String("Laksh");
+        int a=10;
+        System.out.println(s1 instanceof String);
+    }
+}
---
+++ src/ex_04_Operators/Op_Logical.java
@@ -0,0 +1,18 @@
+package ex_04_Operators;
+
+public class Op_Logical {
+    public static void main(String[] args) {
+        boolean a= true;
+        System.out.println(a);
+        System.out.println(!a);
+
+        boolean b= true;
+        System.out.println(!!b);
+
+        boolean c= true || false;
+        System.out.println(c);
+
+        boolean d= true && false;
+        System.out.println(d);
+    }
+}
